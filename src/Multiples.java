47a040baa7f58714479db266014494f370797889
sudharshanpal <sudharshanpalaniyappan12@gmail.com>
2024-09-09 14:17:53 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Multiples {
-}
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                counter++;
+            }
+
+        }
+        System.out.println(counter);
+
+    }
+     }
+
