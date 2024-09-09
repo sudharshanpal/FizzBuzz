8e8a6822ec5fc3ded68b0f37a86b6051ffd8b09d
sudharshanpal <sudharshanpalaniyappan12@gmail.com>
2024-09-09 14:29:27 -0400

    refactored to extract helper method

---
+++ src/Reduce.java
@@ -0,0 +1,17 @@
+public class Reduce {
+
+    public static void main(String[] args) {
+        int n = 100;
+        int counter = 0;
+        while (n != 0) {
+            if (n % 2 == 0)
+                n = n / 2;
+            else {
+                n = n - 1;
+            }
+            counter++;
+        }
+        System.out.println(counter);
+
+    }
+}
